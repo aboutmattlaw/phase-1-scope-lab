bdce3133a524ce35053e188042d3e507753259f9
Matt <aboutmattlaw@gmail.com>
2021-09-05 18:34:29 -0400

    commit tree b4571aeacc7bc1f587f25f9d0ee3c78272b38319

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newFavorite){
+    bestCustomer = 'maybe bob'
+  }
+
+const leastFavoriteCustomer = 'toby';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'matt'
+}
+
+// var leastFavoriteCustomer = 'sour fred'
+
+// function upperCaseCustomerName (){
+//     return customerName = customerName.toUpperCase()
+// }
+
+// function changeLeastFavoriteCustomer(){
+//     leastFavoriteCustomer = 'sour fred'
+//   }
+
+// function setBestCustomer(){
+//     bestCustomer = 'not bob'
+// }
+
+// function overWriteBestCustomer(newFavorite){
+//     bestCustomer = "maybe bob"
+//   }
+
+//   function congratulateCustomer(){
+//     return `congrats ${favoriteCustomer}`
+//   }
+  
+//   function attemptTwoFavoriteCustomers(){
+//       let favoriteCustomer = 'bar'
+//       let favoriteCustomer = 'not bar'
+//   }
